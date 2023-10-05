bd6b680b787cd088cfc9456e77f8304e0915e10a
vovankhoi <khoib1910533@student.ctu.edu.vn>
2023-10-06 00:09:26 +0700

    Cai dat cac handler truy xuat CSDL

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
 const app = require("./app");
 const config = require("./app/config");
+const MongoDB = require("./app/utils/mongodb.util");
 
-// start server
+async function startServer() {
+    try {
+        await MongoDB.connect(config.db.uri);
+        console.log("Connected to the database!");
+
+        // start server
 const PORT = config.app.port;
 app.listen(PORT, () => {
     console.log('server is running on port ${PORT}.');
 });
+    }catch (error) {
+        console.log("cannot connect to the database!", error);
+        process.exit();
+    }
+}
+
+startServer();
